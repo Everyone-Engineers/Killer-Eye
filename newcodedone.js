7303570215bb013203a28443d543561a52821733
Emmanuel Roy <royemmanuel39@gmail.com>
2020-11-06 23:50:32 -0600

    Add files via upload

---
+++ newcodedone.js
@@ -0,0 +1,48 @@
+//Variable for teachable machine model converted and stored
+var imageURL = 'https://teachablemachine.withgoogle.com/models/meX2zcuwY/';
+var network;
+function preload() {
+     network = ml5.imageClassifier(imageURL + 'model.json');
+ }
+// Video and setup the page
+var vid;
+var reversevideo;
+function setup() {
+	createCanvas(640, 480);
+    vid = createCapture();
+    vid.size(640, 480);
+	vid.hide();
+	reversevideo = ml5.flipImage(vid)
+    classifier();
+}
+//label and background work
+var label;
+function draw() {
+     image(vid, 0, 0);
+	 textSize(69);
+	 fill(169);
+     textAlign(CENTER);
+     text(label, width / 2, height - 69);
+}
+//Classfier Function
+function classifier() {
+     reversevideo = ml5.flipImage(vid)
+	 network.classify(reversevideo, gotResult);
+}
+ // When we get a result
+function gotResult(error, results) {
+    if (error) {
+         console.error(error);
+         return;
+    }     
+	 if(results[0].confidence>0.69){
+     label = results[0].label;
+     }
+     else{
+     label = "???";
+     }
+     reversevideo.remove();
+     classifier();
+}
+
+ 
